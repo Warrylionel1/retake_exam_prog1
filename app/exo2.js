c4d2e12f4e7e74bba5d91dd9da90498694413517
warry <hei.warry@gmail.com>
2026-09-18 14:18:52 +0300

    feat(exo2) : implement Ranger function

--- app/exo2.js
+++ app/exo2.js
@@ -7,3 +7,19 @@ export function Echanger(tableau, i, j) {
 
 let test = [0, 1, 0];
 console.log("Résultat: ", Echanger(test, 1, 2));
+
+export function Ranger(T) {
+    let zeroIndex = 0;
+
+    for (let i = 0; i < T.length; i++) {
+        if (T[i] === 0) {
+            Echanger(T, i, zeroIndex);
+            zeroIndex++;
+        }
+    }
+
+    return T;
+}
+
+let test1 = [0, 1, 0, 0, 1, 0];
+console.log("Result : ", Ranger(test1));
